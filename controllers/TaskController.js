b4c56bc1cc44a033b8e2f4ccf5f8cc48e42a3243
vtrolinux <vtrolinux@gmail.com>
2021-12-16 07:44:49 -0300

    delete task

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,9 +1,11 @@
 const Task = require('../models/Task')
 
 module.exports = class TaskController {
+
     static createTask(req, res){
         res.render('tasks/create')
     }
+
     static async createTaskSave(req, res){
 
         const task = {
@@ -17,8 +19,19 @@ module.exports = class TaskController {
         await Task.create(task)
         res.redirect('/tasks')
     }
+
     static async showTasks(req, res){
+
         const tasks = await Task.findAll({raw: true})
         res.render('tasks/all',{tasks})
+
+    }
+    
+    static async removeTask(req, res) {
+
+        const id = req.body.id
+        await Task.destroy({where: {id: id}})
+
+        res.redirect('/tasks')
     }
 }
