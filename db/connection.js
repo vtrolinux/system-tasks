5a62ac5da17d14a8802f3d8752b2caf222d0faa4
vtrolinux <vtrolinux@gmail.com>
2021-12-16 04:01:30 -0300

    first commit

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('tasks', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+  })
+  
+  try {
+    sequelize.authenticate()
+    console.log('Conectamos com o Sequelize!')
+  } catch (error) {
+    console.error('Não foi possível conectar:', error)
+  }
